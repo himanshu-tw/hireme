7720e5222cb3aa622714cec7649c4664e0526a86
Himanshu <ht5620085@gmail.com>
2026-05-14 00:40:12 +0530

    adding auth router

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
+const authRouter = require('./routes/auth.ts')
 
 dotenv.config();
 
@@ -8,9 +9,12 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+
 app.get('/health', (req: any, res: any) => {
   res.json({ status: 'ok' });
 });
+
+app.use('/api/auth', authRouter)
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
