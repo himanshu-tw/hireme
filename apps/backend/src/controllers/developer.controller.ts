6f33851250b9edea00a04315bdff8778dd3ddf86
Himanshu <ht5620085@gmail.com>
2026-06-05 17:48:33 +0530

    createDevProfile done to create a developer profile

--- apps/backend/src/controllers/developer.controller.ts
+++ apps/backend/src/controllers/developer.controller.ts
@@ -25,3 +25,38 @@ export const getDevProfile = async (req: AuthRequest, res: Response) => {
     return res.status(500).json({ message: 'Server error' })
   }
 }
+
+export const createDevProfile = async (req: AuthRequest, res: Response) => {
+  try {
+    const { name, bio, skills, resumeUrl} = req.body;
+
+      if (!name || !bio || !skills || !resumeUrl) {
+        return res.status(400).json({ message: "All fields are required" })
+      }
+
+      const id = req.user?.id;
+
+      if (!id) {
+        return res.status(400).json({ message: "id not found" })
+      }
+
+      const existingProfile = await db.select().from(developerProfiles).where(eq(developerProfiles.userId, id));
+
+      if (existingProfile.length > 0) {
+        return res.status(400).json({ message: "Profile already exists" })
+      }
+
+      const [profile] = await db.insert(developerProfiles).values({
+        userId: id,
+        name,
+        bio,
+        skills,
+        resumeUrl
+      }).returning();
+
+      return res.json({ profile });
+  } catch (err) {
+    console.error(err)
+    return res.status(500).json({ message: 'Server error' })
+  }
+}
