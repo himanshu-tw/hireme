39bffecb22f0847e029a56911824b4a2c39b4273
Himanshu <ht5620085@gmail.com>
2026-06-07 17:34:30 +0530

    adding the verify to the route

--- apps/backend/src/routes/auth.ts
+++ apps/backend/src/routes/auth.ts
@@ -1,7 +1,7 @@
 import express from 'express'
 
 // controllers
-import { register, signIn, me, logout } from "../controllers/auth.controller";
+import { register, signIn, me, logout, verify } from "../controllers/auth.controller";
 import { authenticate } from '../middleware/middleware';
 
 const router = express.Router()
@@ -10,5 +10,6 @@ router.post("/register", register)
 router.post("/sign-in", signIn)
 router.get("/me", authenticate, me)
 router.post("/logout", logout)
+router.get('/verify', verify)
 
 export default router
